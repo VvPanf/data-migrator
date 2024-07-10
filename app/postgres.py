6bd4b3b494668c4f16260e7814048e069f8031e7
User <teshbud@yandex.ru>
2024-07-10 16:10:37 +0300

    Fix postgres

--- app/postgres.py
+++ app/postgres.py
@@ -7,7 +7,7 @@ from app.config import load_config
 
 class PostgresDao:
     def __init__(self, section: str):
-        self.mode = section[-3:-1].upper()
+        self.mode = section[-3:].upper()
         self.config = load_config(section)
         self.schema = self.config.pop('schema')
         self.conn = None
@@ -25,11 +25,11 @@ class PostgresDao:
     def create_table(self, table_name: str, columns_with_types: list):
         cur = self.conn.cursor()
         query = f'drop table if exists {self.schema}.{table_name}'
-        self.log(self.log(query))
+        self.log(query)
         cur.execute(query)
         columns_str = ','.join([column['name'] + ' ' + column['type'] for column in columns_with_types])
         query = f'create table if not exists {self.schema}.{table_name} ({columns_str})'
-        self.log(self.log(query))
+        self.log(query)
         cur.execute(query)
         self.conn.commit()
 
@@ -38,6 +38,6 @@ class PostgresDao:
         columns = ','.join(column_names)
         args_str = '(' + ','.join(['?'] * len(column_names)) + ')'
         query = f'insert into {self.schema}.{table_name} ({columns}) values {args_str}'
-        self.log(self.log(query))
+        self.log(query)
         cur.executemany(query, data)
         self.conn.commit()
